1a8b6efdb4600e49b2d444be63eafcacb042b574
hellattexyss <160811589+hellattexyss@users.noreply.github.com>
2026-02-03 01:59:58 +0530

    Update ab.lua

--- ab.lua
+++ ab.lua
@@ -104,7 +104,7 @@ if game.PlaceId == 10449761463 or game.PlaceId == 130818724007978 or game.PlaceI
     
     local Window = WindUI:CreateWindow({
         Title = "Combat UI - Waspire",
-        Icon = "rbxassetid://125242807265435",
+        Icon = "rbxassetid://122251684068515",
         Author = "The Strongest Battlegrounds",
         Folder = "CombatGUI",
         Size = UDim2.fromOffset(620, 140),
