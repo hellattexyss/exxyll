bd0fb7ff7ec141d56c69dd28ab3f9fdec5df2033
hellattexyss <160811589+hellattexyss@users.noreply.github.com>
2026-02-03 14:04:25 +0530

    Update ab.lua

--- ab.lua
+++ ab.lua
@@ -224,8 +224,6 @@ function AutoBlock:Start()
     
     self.Enabled = true
     self.Blocking = false
-    self.JustBlocked = false  -- Add this line
-    self.M1Cooldown = false   -- Add this line
         
     local heartbeatConn = RunService.Heartbeat:Connect(function()
         if not self.Enabled then return end
@@ -284,20 +282,10 @@ function AutoBlock:Start()
                 self.Blocking = false
                 self:ReleaseBlockKey()
                 
-                if ConfigManager:Get("M1AfterBlockEnabled") and not self.M1Cooldown then
-                    self.M1Cooldown = true
-                    self.JustBlocked = true
-                    
-                    -- M1 once
+                if ConfigManager:Get("M1AfterBlockEnabled") then
                     self:PressM1()
-                    task.wait(0.05)  -- Shorter wait
+                    task.wait(0.1)
                     self:ReleaseM1()
-                    
-                    -- Reset cooldown after a short delay
-                    task.delay(0.5, function()
-                        self.M1Cooldown = false
-                        self.JustBlocked = false
-                    end)
                 end
             end
         end
