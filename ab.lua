1702e8546b3db9dc8ec230555076b8791cc24b49
hellattexyss <160811589+hellattexyss@users.noreply.github.com>
2026-03-13 12:50:52 +0530

    Update ab.lua

--- ab.lua
+++ ab.lua
@@ -1,6 +1,6 @@
 -- Snippet 1/5: Configuration and Setup
 if game.PlaceId == 10449761463 or game.PlaceId == 130818724007978 or game.PlaceId == 131048399685555 then
-    local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/orialdev/WindUI-Boreal/main/WindUI%20Boreal"))()
+    local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/08v3/WindUi-Modified/main/windui.lua"))()
     
     local ConfigManager = {}
     local configFile = "WaspireCombatUI.json"
